999dc5fd0cc0cbb2bb10b86ce14e10ac454cf90e
BorisSlonov <sslonovborisss@gmail.com>
2024-07-16 03:27:19 +0300

    fix

--- src/components/dashboard/Ava/index.tsx
+++ src/components/dashboard/Ava/index.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import { getServerSession } from 'next-auth';
 import { getCurrentUser } from '@/lib/fetchData/getCurrentUser';
 import { authOptions } from '@/app/api/auth/[...nextauth]/authOptions';
+import styles from './Ava.module.css'
 
 export default async function Ava() {
     const session = await getServerSession(authOptions);
@@ -10,6 +11,7 @@ export default async function Ava() {
     return (
         <>
             <Image
+                className={styles.ava}
                 src="https://accishop.ru/assets/v2/img2/logo.svg"
                 width={360}
                 height={80}
